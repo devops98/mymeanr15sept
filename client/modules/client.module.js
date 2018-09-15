01e32938e32ae6a4db3366d0cd6b517ad3cf4816
devopst2020 <satyareddyipl@gmail.com>
2018-09-15 13:00:10 +0100

    added

---
+++ client/modules/client.module.js
@@ -0,0 +1,14 @@
+;(function () {
+  'use strict'
+
+  angular.module('app', [
+    'app.core',
+    'app.index',
+    'app.header',
+    'app.footer',
+    'app.user',
+    'app.blog',
+    'app.admin',
+    'app.chat' // DONT REMOVE - APP GENERATOR
+  ])
+})()
---
+++ tests/unit/mocha.test.js
@@ -0,0 +1,17 @@
+process.env.NODE_ENV = 'nightwatch'
+var Mean = require('../../server.mean.js')
+var run = require('../../run.js')
+describe('MEANSTACKJS API Testing', function () {
+console.log(process.env);
+  before(function (done) {
+    this.timeout(20000)
+    run(Mean, function () {
+      require('../seed.js')(function () {
+        done()
+      })
+    })
+  })
+  require('glob').sync('server/modules/**/*.spec.js').forEach(function (file) {
+    require('../../' + file)
+  })
+})
