3354e77d89c402eb08f9a6fa6983b97d5bfdf654
devops98 <satyareddyipl@gmail.com>
2018-09-16 16:46:03 +0100

    Update development.js

--- configs/environments/development.js
+++ configs/environments/development.js
@@ -1,4 +1,4 @@
-var mongodbUri = process.env.DB_PORT_27017_TCP_ADDR || process.env.MONGODB || process.env.MONGOLAB_URI || 'mongodb://34.244.237.121/dev'
+var mongodbUri = process.env.DB_PORT_27017_TCP_ADDR || process.env.MONGODB || process.env.MONGOLAB_URI || 'mongodb://54.171.166.47/dev'
 module.exports = {
   minify: process.env.MINIFY || 'default', // 'concat' all files or 'minify' concat and minfy  or 'default' leave as is
   html: {
